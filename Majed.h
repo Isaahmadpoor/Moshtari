92baadf49d97cdc2964e484df338ec4d60ad6cf8
Ea7Hsz <164683065+Isaahmadpoor@users.noreply.github.com>
2024-05-10 14:03:40 +0330

    Update and rename 3 to Majed.h

--- Majed.h
+++ Majed.h
@@ -1,4 +1,4 @@
 #profile-title: EisaA7
 #profile-update-interval: 1
-#subscription-userinfo: upload=0; download=0; total=10737418240000000; expire=1717372731
+#subscription-userinfo: upload=0; download=0; total=10737418240000000; expire=1717977531
 warp://162.159.192.118:8742/?ifp=5-10
