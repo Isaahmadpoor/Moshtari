9c57132540c25a262758b426aaafda788456a090
Ea7Hsz <164683065+Isaahmadpoor@users.noreply.github.com>
2024-07-09 03:04:27 +0330

    Update Hadiyan.m

--- Hadiyan.m
+++ Hadiyan.m
@@ -1,4 +1,4 @@
 #profile-title: EisaA7
 #profile-update-interval: 1
-#subscription-userinfo: upload=0; download=0; total=10737418240000000; expire=1720483131
+#subscription-userinfo: upload=0; download=0; total=10737418240000000; expire=1723075131
 warp://162.159.195.69:3138/?ifp=5-10
