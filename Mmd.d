0e728e89c200817f55f3852359b9d7cd5eca98eb
Ea7Hsz <164683065+Isaahmadpoor@users.noreply.github.com>
2024-07-12 23:49:40 +0330

    Update Mmd.d

--- Mmd.d
+++ Mmd.d
@@ -1,4 +1,4 @@
 #profile-title: EisaA7
 #profile-update-interval: 1
-#subscription-userinfo: upload=0; download=0; total=10737418240000000; expire=1721951531
+#subscription-userinfo: upload=0; download=0; total=10737418240000000; expire=1723420331
 warp://162.159.192.137:3581/?ifp=5-10
