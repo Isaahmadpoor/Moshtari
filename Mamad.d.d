d382e491bee5b6a4756eef84792a07c3cfe38ee9
Ea7Hsz <164683065+Isaahmadpoor@users.noreply.github.com>
2024-08-08 14:16:21 +0330

    Update Mamad.d.d

--- Mamad.d.d
+++ Mamad.d.d
@@ -1,1 +1,1 @@
-vless://dc525784-728d-4aa1-af7b-4d4d79ec95b9@100.shecanmarket.com:8443?security=none&encryption=none&host=head.shecanmarket.com&type=tcp&headerType=http&path=%2F#shecan+7329457819
+vless://4f5d358f-615d-4139-a0cb-55ea34864d38@4.shecanmarket.com:8443?security=none&encryption=none&host=head.shecanmarket.com&type=tcp&headerType=http&path=%2F#shecan+7329457819
